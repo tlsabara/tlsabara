7dc86bc464660085f4613f086be12827360f139f
Thiago Sabará <78706759+tlsabara@users.noreply.github.com>
2022-03-21 08:06:30 -0300

    Add files via upload

---
+++ me.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+by Tlsabará
+"""
+class Human:
+    def __init__(self, name):
+        self.alive = True
+        self.name = name
+        self.hungry = 100
+        self.energy = 50
+        self.drams = False
+        self.intelligence = 120
+        self.to_do = 'eat, sleep, code, repeat'
+        
+    def eat(self):
+        self.hungry = 0
+        self.energy = 100
+        return 'Done'
+    
+    def sleep(self):
+        self.dreams = True
+        self.energy = 100
+        return 'Awake'
+    
+    def code(self):
+        self.intelligence += 10
+        self.energy = 10
+        self.hungry = 100
+        
+    def repeat(self):
+        self.to_do = 'new tasks'
+    
+
+
+me = Human
+
+while me.alive == True:
+    me.code()
+    me.eat()
+    me.sleep()
+    me.repeat()
